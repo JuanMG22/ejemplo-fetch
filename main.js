9885cb348766eefd70486f5da562534dcc7fefd5
JuanMG22 <juan.m-gonzalez@outlook.com>
2022-08-28 17:19:54 -0300

    hands on

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
 //Datos que se requieren obtener
 let productos = []
 
-console.warn('Se produjo un error')
+
+
+//Mostrar Error por consola
+console.error("Se produjo un error");
+
+//Mostrar Advertencia por consola
+console.warn("Advertencia");
 
 //Funcion que trae datos utilizando fetch y resolviendolo con async y await
 const getDatos = async () => {
   try {
     const respuesta = await fetch("productos.json");
     productos = await respuesta.json()
+    renderizarProductos()
   } catch (error) {
     console.error("Ha ocurrido un")
   }
@@ -15,10 +22,10 @@ const getDatos = async () => {
 
 getDatos()
 
-let cartas = document.getElementById("cartas");
 
 
 //Renderizo en el DOM los productos
+let cartas = document.getElementById("cartas");
 const renderizarProductos = () => {
   productos.map((producto) => {
     const { imagen, titulo, precio } = producto; //Explicar destructuring
